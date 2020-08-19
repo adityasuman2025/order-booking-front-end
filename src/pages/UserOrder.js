7f8c5ea76f500423fbaadd1d2c34d7a970d0fce3
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-08-19 18:24:14 +0530

    class comp converted to functional comp everywhere

--- src/pages/UserOrder.js
+++ src/pages/UserOrder.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import CircularButton from "../components/CircularButton";
 import SnackBar from "../components/SnackBar";
@@ -7,141 +7,131 @@ import LoadingAnimation from "../components/LoadingAnimation";
 import { checkUserExistsWithGivenPhoneNumber } from "../apis";
 import { getDecryptedCookieValue, makeEncryptedCookie } from "../utils";
 
-class UserOrder extends Component {
-    constructor(){
-	    super();
-	   
-	    this.state = {
-            loading: true,
-
-			productID: 0,
-
-			snackBarVisible: false,
-			snackBarMsg: "",
-            snackBarType: "success",
-            
-            enteredPhoneNo: 0,
-        };
-  	}
-
-  	componentDidMount = async () => {
-	//by default first page of products will be listed
-        const product_id = this.props.match.params.product_id;
-        await this.setState({
-            productID: product_id,
-            enteredPhoneNo: await getDecryptedCookieValue( "order_booking_user_phone_no" ) || 0,
-        });
-
-		await this.toogleLoadingAnimation(); //hiding loading animation
-    }
+function UserOrder(props) {
+    const [ loading, setLoading ] = useState( true );
     
+    const [ snackBarVisible, setSnackBarVisible ] = useState( false );
+    const [ snackBarMsg, setSnackBarMsg ] = useState( "" );
+    const [ snackBarType, setSnackBarType ] = useState( "success" );
+    
+    const [ productID, setProductID ] = useState( 0 );
+	
+	const [ enteredPhoneNo, setEnteredPhoneNo ] = useState( "" );
+
+//componentDidMount
+    useEffect( () => {
+        const componentDidMount = async () => {
+        //by default first page of products will be listed
+            const product_id = props.match.params.product_id;
+            const entered_PhoneNo = await getDecryptedCookieValue( "order_booking_user_phone_no" ) || "";
+            
+            setProductID( product_id );
+            setEnteredPhoneNo( entered_PhoneNo );
+
+            await hideLoadingAnimation(); //hiding loading animation
+        }
+
+        componentDidMount();
+    }, []);
+
 //function to make a snack-bar
-    makeSnackBar = ( msg, type ) => {
-        this.setState({
-            snackBarVisible: true,
-            snackBarMsg: msg,
-            snackBarType: type,
-        });
+    const makeSnackBar = async ( msg, type ) => {
+        await setSnackBarMsg( msg );
+        await setSnackBarType( type );
+
+        await setSnackBarVisible( true );
     }
 
 //function to close snack-bar
-    handleSnackBarClose = () => {
-        this.setState({
-            snackBarVisible: false
-        });
+    const handleSnackBarClose = () => {
+        setSnackBarVisible( false );
     }
 
 //function to toogle loadiing animation
-    toogleLoadingAnimation = ( ) => {
-        this.setState({
-            loading: !this.state.loading,
-        });
+    const displayLoadingAnimation = async () => {
+        await setLoading( true );
+    }
+
+    const hideLoadingAnimation = async () => {
+        await setLoading( false );
     }
 
 //when proceed btn is pressed
-    onProceedPress = async (e) => {
+    const onProceedPress = async (e) => {
         e.preventDefault();
 
-        await this.toogleLoadingAnimation(); //showing loading animation
+        await displayLoadingAnimation(); //showing loading animation
 
     //checking if some phone no is entered 
-        const enteredPhoneNo = this.state.enteredPhoneNo;
-        if( enteredPhoneNo == 0 ) {
-            await this.makeSnackBar( "Please enter a phone number", "error" );
+        const entered_PhoneNo = await enteredPhoneNo.trim();
+        if( entered_PhoneNo == "" ) {
+            await makeSnackBar( "Please enter a phone number", "error" );
         } else {
-            const response = await checkUserExistsWithGivenPhoneNumber( enteredPhoneNo );
+            const response = await checkUserExistsWithGivenPhoneNumber( entered_PhoneNo );
             if( response == 1 ) {
-                const product_id = await this.state.productID;;
-
             //making of the phone no entered by user and redirecting to book-order page
-                const enteredPhoneNo_cookie = await makeEncryptedCookie( "order_booking_user_phone_no", enteredPhoneNo );
+                const enteredPhoneNo_cookie = await makeEncryptedCookie( "order_booking_user_phone_no", entered_PhoneNo );
                 if( enteredPhoneNo_cookie ) {
-                    this.props.history.push( '/user/book-order/' + product_id );
+                    const product_id = await productID;
+                    props.history.push( '/user/book-order/' + product_id );
 
                     return;
                 } else {
-                    await this.makeSnackBar( "Something went wrong", "error" );    
+                    await makeSnackBar( "Something went wrong", "error" );    
                 }
             } else if ( response == 0 ){
-                await this.makeSnackBar( "This phone number is not registered. Please Create a User to continue", "error" );
+                await makeSnackBar( "This phone number is not registered. Please Create a User to continue", "error" );
             } else {
-                await this.makeSnackBar( "Something went wrong", "error" );
+                await makeSnackBar( "Something went wrong", "error" );
             }
         }
-        await this.toogleLoadingAnimation(); //hiding loading animation
-    }
-
-//when an input box content is changed in add question dialog box
-    onChange = (e) => {
-        this.setState({[e.target.name]:e.target.value});
+        await hideLoadingAnimation(); //hiding loading animation
     }
 
 //rendering
-    render() {
-        return (
-            <div>
-                <div className="pageContent center">
-                    <br /><br /><br /><br />
-                    <h2>
-                        Book Your Order
-                    </h2>
-                    <br />
+    return (
+        <div>
+            <div className="pageContent center">
+                <br /><br /><br /><br />
+                <h2>
+                    Book Your Order
+                </h2>
+                <br />
 
-                    <form onSubmit={ this.onProceedPress }>
-                        <label>
-                            enter your phone number
-                            <br />
-                            <input 
-                                type="number" 
-                                className="inputBox" 
-                                placeholder="phone number" 
-                                name="enteredPhoneNo" 
-                                value={ this.state.enteredPhoneNo }
-                                onChange={ this.onChange } />
-                        </label>
-                        <br /><br />
+                <form onSubmit={ onProceedPress }>
+                    <label>
+                        enter your phone number
+                        <br />
+                        <input 
+                            type="number" 
+                            className="inputBox" 
+                            placeholder="phone number" 
+                            name="enteredPhoneNo" 
+                            value={ enteredPhoneNo }
+                            onChange={ ( e ) => setEnteredPhoneNo( e.target.value ) } />
+                    </label>
+                    <br /><br />
 
-                        <CircularButton text="Proceed" onClick={ this.onProceedPress }/>
-                    </form>
-                    <br />
-                    <button 
-                        className="btn coloredTextBtn"
-                        onClick={ () => { this.props.history.push( '/user/create-user' ) } }
-                    >
-                        CREATE USER
-                    </button>
+                    <CircularButton text="Proceed" onClick={ onProceedPress }/>
+                </form>
+                <br />
+                <button 
+                    className="btn coloredTextBtn"
+                    onClick={ () => { props.history.push( '/user/create-user' ) } }
+                >
+                    CREATE USER
+                </button>
 
-                    <LoadingAnimation loading={ this.state.loading } />
-                </div>
-
-                <SnackBar 
-					open={ this.state.snackBarVisible } 
-					msg={ this.state.snackBarMsg } 
-					type={ this.state.snackBarType }
-					handleClose={ this.handleSnackBarClose } />
+                <LoadingAnimation loading={ loading } />
             </div>
-        );
-    }
+
+            <SnackBar 
+                open={ snackBarVisible } 
+                msg={ snackBarMsg } 
+                type={ snackBarType }
+                handleClose={ handleSnackBarClose } />
+        </div>
+    )
 }
 
 export default UserOrder;
