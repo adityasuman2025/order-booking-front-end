84e0a1150a2c69aa5830c287fa863307805f8fa9
adityasuman2025 <aditya.me16@iitp.ac.in>
2020-08-19 10:49:40 +0530

    AdminNavBar compon. changed to functional component

--- src/components/AdminNabBar.js
+++ src/components/AdminNabBar.js
@@ -1,51 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 
 import { project_name } from "../constants";
 
-class AdminNabBar extends Component {
-	constructor(){
-	    super();
-	   
-	    this.state = {
-		    mobileNavBarToggler: false,
-	    };
-  	}
+function AdminNabBar(props) {
+	const [ mobileNavBarToggler, setMobileNavBarToggler ] = useState( false );	
 
 //function to toggle mobile menu 
-	handleMobileNavBarToggler = () => {
-		this.setState({
-			mobileNavBarToggler: !this.state.mobileNavBarToggler
-		});
+	const handleMobileNavBarToggler = () => {
+		setMobileNavBarToggler( !mobileNavBarToggler );
 	}
 
 //rendering
-	render() {
-		return(
-			<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark noPadding" >
-				<div id="navbar-brand" className="navbar-brand navBarBrand">
-					<img className="navLogoImg" alt="logo image" src={ require("../img/logo.png") } /> { project_name }
-				</div>
+	return(
+		<nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark noPadding" >
+			<div id="navbar-brand" className="navbar-brand navBarBrand">
+				<img className="navLogoImg" alt="logo image" src={ require("../img/logo.png") } /> { project_name }
+			</div>
 
-				<button id="mobileNavBarToggler" onClick={ this.handleMobileNavBarToggler } className="navbar-toggler" >
-			        <span className="navbar-toggler-icon"></span>
-		      	</button>
+			<button id="mobileNavBarToggler" onClick={ handleMobileNavBarToggler } className="navbar-toggler" >
+				<span className="navbar-toggler-icon"></span>
+			</button>
 
-				<div
-					id="navbarCollapse"
-				  	className={ classNames( 'navbar-collapse', { 'collapse': !this.state.mobileNavBarToggler }, { 'display': this.state.mobileNavBarToggler } ) }
-					// className={ this.state.mobileNavBarToggler === false ? "collapse " : "display" }
-				>
-		      		<ul className="navbar-nav mr-auto navWebMenu" >
-	      				<li className={ this.props.active === "dashboard" ? "nav-item active" : "nav-item" } ><Link className="nav-link" to="/admin/dashboard">Dashboard</Link></li>
-						<li className={ this.props.active === "filter" ? "nav-item active" : "nav-item" } ><Link className="nav-link"  to="/admin/filter">Filter</Link></li>
-						<li className={ this.props.active === "create_product" ? "nav-item active" : "nav-item" } ><Link className="nav-link"  to="/admin/create-product">Create Product</Link></li>
-		      		</ul>
-		      	</div>
-			</nav>
-		)
-	}
+			<div
+				id="navbarCollapse"
+				className={ classNames( 'navbar-collapse', { 'collapse': !mobileNavBarToggler }, { 'display':mobileNavBarToggler } ) }
+				// className={ this.state.mobileNavBarToggler === false ? "collapse " : "display" }
+			>
+				<ul className="navbar-nav mr-auto navWebMenu" >
+					<li className={ classNames( "nav-item", { "active": props.active == "dashboard" } ) }>
+						<Link className="nav-link" to="/admin/dashboard">Dashboard</Link>
+					</li>
+					<li className={ classNames( "nav-item", { "active": props.active == "filter" } ) }>
+						<Link className="nav-link"  to="/admin/filter">Filter</Link>
+					</li>
+					<li className={ classNames( "nav-item", { "active": props.active == "create_product" } ) }>
+						<Link className="nav-link"  to="/admin/create-product">Create Product</Link>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	)
 }
 
 export default AdminNabBar;
